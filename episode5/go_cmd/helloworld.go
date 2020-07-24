8a68ff3378560a83842855684690b7ddb2bec402
Vietwear <nguyenvu_it123@yahoo.com>
2020-07-23 22:39:28 -0700

    username

--- episode5/go_cmd/helloworld.go
+++ episode5/go_cmd/helloworld.go
@@ -5,7 +5,7 @@ import "flag"
 var intflagvar int
 var stringflagvar string
 var boolvar bool
-
+//var
 func main() {
 	flag.IntVar(&intflagvar, "integer flag", 9090, "help message for this flag")
 	flag.StringVar(&stringflagvar, "name of flag", "default value", "help message for this flag")
